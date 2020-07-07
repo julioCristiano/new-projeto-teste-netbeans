6ef552d3164d60973255b84d11d8695312ce0c85
julio cristiano <juliocristiano@outlook.com>
2020-07-07 20:08:13 -0300

    segundo commit

--- Documents/NetBeansProjects/exemplo-git-netbeans/src/exemplo/git/netbeans/ExemploGitNetbeans.java
+++ Documents/NetBeansProjects/exemplo-git-netbeans/src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -17,6 +17,7 @@ public class ExemploGitNetbeans {
     public static void main(String[] args) {
         System.out.println("Exemplo de um projeto com git e netbeans.");
         //alterado da web para netbeans.
+        //alterado no netbeans para a web
     }
     
 }
